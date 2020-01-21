9e7c5a39ca1605a80983eb76d14aed3ac6c43fa8
chrisdangnguyen <cdnguyen24@gmail.com>
2020-01-20 20:18:57 -0800

    authentication

--- app.js
+++ app.js
@@ -2,19 +2,32 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
-mongoose
-  .connect(db, { useNewUrlParser: true})
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
-
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+// const User = require('./models/User');
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+  
+  app.use(bodyParser.urlencoded({ extended: false}));
+  app.use(bodyParser.json());
+
+  // app.get("/", (req, res) => {
+  //   const user = new User({
+  //     handle: "jim",
+  //     email: "jim@jim.jim",
+  //     password: "jimisgreat123"
+  //   })
+  //   user.save
+  //   res.send("Hello World");
+  // });
+
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
 
 
 app.get("/", (req, res) => res.send("Hello World"));
